f0b81d0f532876c1bb81c55faffd72e109613511
Hassaan-Naveed <shadowblade056@gmail.com>
2021-02-19 18:55:56 +0000

    Began Implementation of Message sending function in MainWindow.cpp

--- Project Diagrams/GUI Design/mainwindow.cpp
+++ Project Diagrams/GUI Design/mainwindow.cpp
@@ -1,6 +1,8 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
 
+#include <QtWidgets/QMessageBox>
+
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
@@ -13,3 +15,7 @@ MainWindow::~MainWindow()
     delete ui;
 }
 
+void MainWindow::on_sendButton_clicked()
+{
+	ui->messageLog->insertPlainText("uwu");
+}
