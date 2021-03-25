5288809a57cf7b51912157772e826d95647e0f5a
NickMcCaig <N0787115@my.ntu.ac.uk>
2021-03-25 20:16:38 +0000

    Updated Consts quality
    Removed un-needed declerations to make the code look cleaner.

--- Project Implementation/Messaging App Codebase/headers/consts.h
+++ Project Implementation/Messaging App Codebase/headers/consts.h
@@ -7,32 +7,34 @@
 namespace Consts {
 
     struct errors{
-        const std::string error = "Error";
-        const std::string noRoomSelected = "Error: No Room Selected";
-        const std::string noChannelSelected = "Error: No Channel Selected";
-        const std::string subscription = "Could not subscribe. Is there a valid connection?";
-        const std::string sendMessage = "Error: Could not Send Message";
-        const std::string emptyUser = "Error: Username Field Empty";
-        const std::string userNotFound = "Error: Username Not Found";
-        const std::string userSubscribed = "Error: User Already Subscribed";
-        const std::string invalidCreds = "Login Credentials Invalid";
-        const std::string emptyFields = "Error: Fields must not be empty";
+        const std::string
+        error = "Error",
+        noRoomSelected = "Error: No Room Selected",
+        noChannelSelected = "Error: No Channel Selected",
+        subscription = "Could not subscribe. Is there a valid connection?",
+        sendMessage = "Error: Could not Send Message",
+        emptyUser = "Error: Username Field Empty",
+        userNotFound = "Error: Username Not Found",
+        userSubscribed = "Error: User Already Subscribed",
+        invalidCreds = "Login Credentials Invalid",
+        emptyFields = "Error: Fields must not be empty";
     }errors;
     struct buttons{
-        const std::string connect = "Connect";
-        const std::string disconnect = "Disconnect";
-        const std::string username = "Username:";
-        const std::string roomname = "Room Name:";
+        const std::string
+        connect = "Connect",
+        disconnect = "Disconnect",
+        username = "Username:",
+        oomname = "Room Name:";
 
     }buttons;
     struct dialogs{
-        const std::string enterroomname =  "Enter Room Name";
-        const std::string enterChannelName = "Enter Channel Name";
-        const std::string channelName = "Channel Name:";
-        const std::string noChannel = "no-channel-connected";
-        const std::string noGroup = "no-group-selected";
-        const std::string enterUsername = "Enter Username";
-
+        const std::string
+        enterroomname =  "Enter Room Name",
+        enterChannelName = "Enter Channel Name",
+        channelName = "Channel Name:",
+        noChannel = "no-channel-connected",
+        noGroup = "no-group-selected",
+        enterUsername = "Enter Username";
     }dialogs;
 
 }
